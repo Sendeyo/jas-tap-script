4e6b161daa5191bfa05ed13df359db3183fd8eba
Sendeyo <maxmuthomi@gmail.com>
2025-04-13 12:16:30 +0100

    Added local ip

--- nfc.py
+++ nfc.py
@@ -72,7 +72,7 @@ def control(color_str, duration_ms):
 # Function to handle tapping the card
 def tap(card):
     url = "http://127.0.0.1:8000/tap"  # Your server URL
-    url = "http://192.168.100.41:8000/tap"  # Your server URL
+    #url = "http://192.168.100.41:8000/tap"  # Your server URL
 
     # Sending the card data to the server
     data = {"device": "Entrance", "card": str(card)}
